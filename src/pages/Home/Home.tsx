d47e6d9e0602465bb6ce172d68d9bcb6849f273f
Gabriel Estevam <gabriel.estevam.narciso@gmail.com>
2024-05-31 14:10:03 -0300

    Add the hook forms. And valid inputs with zod

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,17 +8,61 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './Home.styles'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+/**
+ * Zod é uma biblioteca de validação de modelo de dados.
+ * O react-hook-form não consegue validar os dados de um formulário por si,
+ * então é necessário de uma biblioteca de validação.
+ *
+ * zod.object - Cria um objeto de validação.
+ */
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, { message: 'Informe o nome da tarefa' }),
+  minutesAmount: zod
+    .number()
+    .min(5, { message: 'O ciclo precisa ser de no mínimo 5 minutos' })
+    .max(60, { message: 'O ciclo precisa ser de no máximo 60 minutos' }),
+})
+
+/**
+ * zod.infer - Função que ajuda a inferir o tipo de dados do formulário a partir do schema de validação.
+ */
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 function Home() {
+  /**
+   * register - Função do react-hook-form que registra um input do formulário
+   * quando não se possui um objeto do modelo predefinido de formulário.
+   *
+   * handleSubmit - Função do react-hook-form que recebe uma função de callback
+   * que será executada quando o formulário for submetido.
+   */
+  const { register, handleSubmit, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em:</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -35,6 +79,7 @@ function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -46,7 +91,7 @@ function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
